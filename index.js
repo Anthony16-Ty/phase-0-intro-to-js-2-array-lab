c993dea1276b021a40b8856a179d2a547f48e553
Anthony Kimeu <tkymeu@gmail.com>
2022-06-05 23:54:57 +0300

    commit tree f9a5697248090b0c8083a88cb93107e79d3eb548

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+   }
+   function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+  }
+  
+  function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+  }
+  
+  function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+  }
+  
+  function appendCat(name){
+    var a= [...cats,name];
+    return a;
+  }
+  
+  function prependCat(name){
+    var b=[name, ...cats];
+    return b;
+  }
+  
+  function removeLastCat(){
+    var catsA= cats.slice(0,cats.length-1);
+    return catsA;
+  }
+  
+  function removeFirstCat(){
+    var catsA= cats.slice(1)
+    return catsA;
+  }
+
+   
+
+
